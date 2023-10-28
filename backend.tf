eb1ff40d283f1f591bf0cebc5e691e0165a535b8
harshalgude <140896754+harshalgude@users.noreply.github.com>
2023-10-27 22:10:06 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-tf-state-bucket-project-terraform-baloteli"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "dynamo-cr"
   }
 }
